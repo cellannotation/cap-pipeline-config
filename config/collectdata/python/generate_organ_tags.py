ffdb5e599c627f1862c6a7121dd77233e8747ca6
bvarner-ebi <bvarner@post.harvard.edu>
2022-03-29 07:15:42 -0400

    Update generate_organ_tags.py

--- config/collectdata/python/generate_organ_tags.py
+++ config/collectdata/python/generate_organ_tags.py
@@ -3,7 +3,7 @@
 
 # ACTION: 
 #  Generate list of IRIs and labels from UBERON organ_slim and output in JSON format
-#  Output will be used to boost search results in Cell Annotation Platform (CAP) 
+#  Output will be used to filter and/or boost search results in Cell Annotation Platform (CAP) 
 
 import sys
 from ruamel.yaml import YAML
@@ -16,13 +16,8 @@ sparql.setReturnFormat(JSON)
 
 
 sparql.setQuery("""
-prefix oio: <http://www.geneontology.org/formats/oboInOwl#>
-prefix def: <http://purl.obolibrary.org/obo/IAO_0000115>
-prefix owl: <http://www.w3.org/2002/07/owl#>
-prefix inSubset: <http://www.geneontology.org/formats/oboInOwl#inSubset>
-prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX overlaps: <http://purl.obolibrary.org/obo/RO_0002131>
-
+PREFIX inSubset: <http://www.geneontology.org/formats/oboInOwl#inSubset>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
 SELECT  ?x ?xLabel
 WHERE 
@@ -35,6 +30,7 @@ WHERE
 
 ret = sparql.queryAndConvert()
 
+# uncomment to view query results
 # for r in ret["results"]["bindings"]:
 #    print(r)
 
@@ -42,6 +38,7 @@ queryOutput = []
 for line in ret["results"]["bindings"]:
         queryOutput.append(line)
 
+# generate list of IRIs and organ labels
 organs = []
 i = 0
 for n in queryOutput:
@@ -51,17 +48,17 @@ for n in queryOutput:
     organs.append((IRI, label))
     i += 1
 
-inp = """\
-neo_node_labelling:
-  classes: x
-  label: y
-"""
-
-yaml = YAML()
-code = yaml.load(inp)
+# generate dictionary and populate with organ IRIs and labels
+organ_labels = {"neo_node_labelling": []}
 i = 0
 for organ in organs:
-  code['neo_node_labelling']['classes'] = organs[i][0]
-  code['neo_node_labelling']['label'] = organs[i][1]
-  yaml.dump(code, sys.stdout)
+  a = {'classes': 
+          [organs[i][0]],
+      'label' : organs[i][1]}
+  organ_labels['neo_node_labelling'].append(a)
   i += 1
+
+# export populated dictionary to file
+# ? can neo4j2owl-config.yaml point to this file?
+with open(r'organ_labels.yaml', 'w') as file:
+    documents = yaml.dump(organ_labels, file)
