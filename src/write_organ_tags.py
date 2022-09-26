9acb208701ae85354142d2916e1b302dbe6812a5
bvarner-ebi <95643797+bvarner-ebi@users.noreply.github.com>
2022-09-26 16:43:28 +0100

    Addresses #81

--- src/write_organ_tags.py
+++ src/write_organ_tags.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# generate_organ_tags.py v1.0.0
+# generate_organ_tags.py v2022.09
 
 # ACTION:
 #  Generate list of organ cell DL queries and labels derived from UBERON organ_slim and send output to YAML file
@@ -24,8 +24,9 @@ def generate_organ_cells(output):
     for n in output:
         curie = n['x']['value'].replace("_", ":")
         curie = curie.partition('http://purl.obolibrary.org/obo/')[-1]
-        # RO:0002131 -> 'overlaps'
-        dl_query = "RO:0002131 some " + curie
+        # CL:0000000 = 'cell'
+        # BFO:0000050 = 'part of'
+        dl_query = "CL:0000000 and BFO:0000050 some " + curie
         label = n['xLabel']['value'].replace(" ", "_")
         organs.append((dl_query, label))
     return organs
@@ -33,14 +34,19 @@ def generate_organ_cells(output):
 
 parser = argparse.ArgumentParser(description = 'set destination YAML file for query output')
 
-parser.add_argument('-f', '--file', default = '../config/prod/neo4j2owl-config.yaml', help = '''
-    Use this option to indicate destination file for organ cell DL queries and semantic labels. By default, output
+parser.add_argument('-i', '--inputfile', default = '../config/prod/neo4j2owl-config_manual.yaml', help = '''
+    Use this option to indicate destination (input) file for organ cell DL queries and semantic labels. By default, query output
     is sent to a file named neo4j2owl-config.yaml.
+    ''')
+parser.add_argument('-o', '--outputfile', default = '../config/prod/neo4j2owl-config.yaml', help = '''
+    Use this option to indicate the final filename for inputfile + organ cell DL queries and semantic labels. By default, the final
+    output file is named neo4j2owl-config.yaml.
     ''')
 
 args = parser.parse_args()
 
-file_name = args.file
+input_file_name = args.inputfile
+output_file_name = args.outputfile
 
 organ_query = """
 PREFIX inSubset: <http://www.geneontology.org/formats/oboInOwl#inSubset>
@@ -57,6 +63,7 @@ WHERE
 # <http://purl.obolibrary.org/obo/uberon/core#organ_slim>
 query_output = utils.run_query(organ_query)
 
+
 # generate list of organ cell DL queries and semantic labels
 organ_list = generate_organ_cells(query_output)
 
@@ -64,11 +71,11 @@ organ_list = generate_organ_cells(query_output)
 yaml = ruamel.yaml.YAML()
 yaml.indent(sequence=4, offset=2)
 
-with open(file_name) as file:
+with open(input_file_name) as file:
     yaml_config = yaml.load(file)
 
 yaml_config['neo_node_labelling'] = utils.update_neo_node_labelling(yaml_config['neo_node_labelling'], organ_list)
 
 # export populated dictionary to file
-with open(file_name, 'w') as file:
+with open(output_file_name, 'w') as file:
     documents = yaml.dump(yaml_config, file)
